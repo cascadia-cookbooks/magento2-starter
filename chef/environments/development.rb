7f519b00dc5d9a1f366eca8ed74091ae04fe1e40
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-28 15:28:16 -0700

    Set attributes for Magento directory ownership

--- chef/environments/development.rb
+++ chef/environments/development.rb
@@ -2,5 +2,11 @@ name 'development'
 description 'Local development environment configuration'
 
 override_attributes(
-
+    # Cascadia Cookbook: 'magento'
+    cas_magento: {
+        directories: {
+            owner: 'vagrant',
+            group: 'vagrant'
+        }
+    }
 )
