7b0cc276682e53b981687bd340fe5c508e3c3018
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-04 16:31:55 -0700

    Magento cron job

--- chef/environments/development.rb
+++ chef/environments/development.rb
@@ -2,6 +2,20 @@ name 'development'
 description 'Local development environment configuration'
 
 override_attributes(
+    # Cascadia Cookbook: 'cron'
+    cas_cron: {
+        jobs: {
+            magento_cron_run: {
+                environment: {
+                    HOME: '/vagrant/magento'
+                    PATH: '/usr/bin',
+                },
+                predefined_value: '* * * * *',
+                user: 'vagrant',
+                command: "php bin/magento cron:run | grep -v 'Ran jobs by schedule' >> var/log/magento.cron.log"
+            }
+        }
+    },
     # Cascadia Cookbook: 'magento'
     cas_magento: {
         directories: {
